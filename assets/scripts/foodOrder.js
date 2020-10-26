083e227acbb351280e184c72f59f1419b576f965
HannePrietz <hanne7907@hotmail.com>
2020-10-26 22:55:19 +0100

    order

--- assets/scripts/foodOrder.js
+++ assets/scripts/foodOrder.js
@@ -1,11 +1,30 @@
+//Antall retter orderedDishes
 function addOrder(){
   var food = document.createElement("el")
   var inputValue = document.getElementById("").value;
+  var t = document.createTextNode(inputValue);
+  el.appendChild(t);
+
+
+  for (var i = 0; i < order_ammount.length; i++) {
+    order_ammount[i]
+    return "order_ammount" in document.createElement
+  }
+  button.addEventListener("onclick", addOrder);
+  button.addEventListener("onclick", function(event) {
+    event.preventDefault();
+  });
+  if (inputValue === "") {
+    total.textContent = +total.textContent + 0;
+  }
+  else {
+    total.textContent = +total.textContent + 1;
+  }
 
 }
 
 
-
+//Totalsum totalPrice
 const order = myOrder();{
 
 }
