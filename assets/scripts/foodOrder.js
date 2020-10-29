320f6d84b21f43c125ed7554d43422d381e5611d
Marcus Benjamin Ravnå Birkeland <36818485+Zprite@users.noreply.github.com>
2020-10-29 13:14:34 +0100

    Made core functionality of foodOrder

--- assets/scripts/foodOrder.js
+++ assets/scripts/foodOrder.js
@@ -1,5 +1,5 @@
-//Måten jeg hadde gjort det på er å ha en onChange som søker etter retten i items.js listen basert på ID, og deretter kan du finne prisen og navnet
-//I items.js filen så har hver enkelt rett en id property. Hver input på siden blir så merket med #item_[id]:
+// Dette er en global variabel som til enhver tid har alle retter i bestillingen.
+var dishes = [];
 
 //Navn på rettene addName() - din ordre #item_[id]
 function addName(){
@@ -26,11 +26,77 @@ function addOrder(){
     total.textContent = +total.textContent + 1;
   }
 }
-//Totalsummen totalPrice
-function totalPrice(){
-  var retter = [];
-  //console.log(retter.reduce((a, b,osv) => a + b, 0));
+
+//Henter totalprisen basert på dishes[]
+function setTotalPrice(){
+  let sum = 0;
+  for (d of dishes){
+    sum+= d.price;
+  }
+  document.querySelector("#totalPrice").innerHTML = sum;
 }
+
+function setNumDishes(){
+  let num = dishes.length;
+  document.querySelector("#orderedDishes").innerHTML = num;
+}
+
+// Denne funksjonen må kalles hver gang vi rendrer rettene
+// Oppdater handlekurv i eventlistner funksjonen.
+function queryInput(){
+  document.querySelectorAll('.orderInput').forEach(item => {
+    item.addEventListener('change', event => {
+      let val = event.target.value;
+      let id =  event.target.id.replace( /^\D+/g, '');
+      updateDishes(id,val);
+      setTotalPrice();
+      setNumDishes();
+    })
+  })
+}
+
+function updateDishes(id,ammount){
+  let pop_i = []
+  // Finner indecies til retter i dishes med samme id
+  for (i = 0; i<dishes.length;i++){
+    if(dishes[i].id == id){
+      pop_i.push(i);
+    }
+  }
+  // Fjerner eksisterende retter fra listen
+  for (i = pop_i.length-1; i >= 0; i--){
+    dishes.splice(pop_i[i],1);
+  }
+  // Legger til riktig antall retter i listen
+  for(i = 0; i<ammount;i++){
+    dishes.push(getFoodById(id));
+  }
+  console.log(dishes);
+}
+
+function getFoodById(id){
+  for (food of antipasti){
+    if (food.id == id){
+      return food
+    }
+  }
+  for (food of paste){
+    if (food.id == id){
+      return food
+    }
+  }
+  for (food of dolci){
+    if (food.id == id){
+      return food
+    }
+  }
+  for (food of pizze){
+    if (food.id == id){
+      return food
+    }
+  }
+}
+
 
 //for (var i = 0; i < order_ammount.length; i++) {
   //order_ammount[i]
