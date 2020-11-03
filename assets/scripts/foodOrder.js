e1ffb6ed0611ef9c4ad651a070884adbf568b735
henrikholen <henriksolheimholen@gmail.com>
2020-11-03 19:39:31 +0100

    Update foodOrder.js

--- assets/scripts/foodOrder.js
+++ assets/scripts/foodOrder.js
@@ -11,119 +11,81 @@ function spisHer() { // kvittering
 }
 
 //Henter navnene på rettene - #orderedDishes".name
-function addName(){
-  var li = document.createElement("li")
-  var rett = document.getElementById(dishes).name;
-  var t = document.createTextNode(rett);
-  var rett = li.appendChild(t);
-  //document.getElementById("dishes").prepend(li); //item_name item_name[i].name
-  document.querySelector("#dishes").innerHTML= rett;
+function addName() {
+    var li = document.createElement("li")
+    var rett = document.getElementById(dishes).name;
+    var t = document.createTextNode(rett);
+    var rett = li.appendChild(t);
+    //document.getElementById("dishes").prepend(li); //item_name item_name[i].name
+    document.querySelector("#dishes").innerHTML = rett;
 }
 
 //Henter totalprisen basert på dishes[]
-function setTotalPrice(){
-  let sum = 0;
-  for (d of dishes){
-    sum+= d.price;
-  }
-  document.querySelector("#totalPrice").innerHTML = sum;
+function setTotalPrice() {
+    let sum = 0;
+    for (d of dishes) {
+        sum += d.price;
+    }
+    document.querySelector("#totalPrice").innerHTML = sum;
 }
 
-function setNumDishes(){
-  let num = dishes.length;
-  document.querySelector("#orderedDishes").innerHTML = num;
+function setNumDishes() {
+    let num = dishes.length;
+    document.querySelector("#orderedDishes").innerHTML = num;
 }
 
 // Denne funksjonen må kalles hver gang vi rendrer rettene
 // Oppdater handlekurv i eventlistner funksjonen.
-function queryInput(){
-  document.querySelectorAll('.orderInput').forEach(item => {
-    item.addEventListener('change', event => {
-      let val = event.target.value;
-      let id =  event.target.id.replace( /^\D+/g, '');
-      updateDishes(id,val);
-      setTotalPrice();
-      setNumDishes();
+function queryInput() {
+    document.querySelectorAll('.orderInput').forEach(item => {
+        item.addEventListener('change', event => {
+            let val = event.target.value;
+            let id = event.target.id.replace(/^\D+/g, '');
+            updateDishes(id, val);
+            setTotalPrice();
+            setNumDishes();
+        })
     })
-  })
 }
 
-function updateDishes(id,ammount){
-  let pop_i = []
-  // Finner indecies til retter i dishes med samme id
-  for (i = 0; i<dishes.length;i++){
-    if(dishes[i].id == id){
-      pop_i.push(i);
-    }
-  }
-  // Fjerner eksisterende retter fra listen
-  for (i = pop_i.length-1; i >= 0; i--){
-    dishes.splice(pop_i[i],1);
-  }
-  // Legger til riktig antall retter i listen
-  for(i = 0; i<ammount;i++){
-    dishes.push(getFoodById(id));
-  }
-  console.log(dishes);
-}
-
-function getFoodById(id){
-  for (food of antipasti){
-    if (food.id == id){
-      return food
-    }
-  }
-  for (food of paste){
-    if (food.id == id){
-      return food
-    }
-  }
-  for (food of dolci){
-    if (food.id == id){
-      return food
-    }
-  }
-  for (food of pizze){
-    if (food.id == id){
-      return food
-    }
-  }
-}
-
-    function modalHouse() {
-        // Get the modal
-        var modalHouseElem = document.getElementById("modalHouseElem");
-        // Get the <span> element that closes the modal
-        var span = document.getElementsByClassName("close")[0];
-        // When the user clicks the button, open the modal
-        modalHouseElem.style.display = "block";
-        // When the user clicks on <span> (x), close the modal
-        span.onclick = function() {
-                modalHouseElem.style.display = "none";
-            }
-            // When the user clicks anywhere outside of the modal, close it
-        window.onclick = function(event) {
-            if (event.target == modalHouseElem) {
-                modalHouseElem.style.display = "none";
-            }
+function updateDishes(id, ammount) {
+    let pop_i = []
+        // Finner indecies til retter i dishes med samme id
+    for (i = 0; i < dishes.length; i++) {
+        if (dishes[i].id == id) {
+            pop_i.push(i);
         }
     }
+    // Fjerner eksisterende retter fra listen
+    for (i = pop_i.length - 1; i >= 0; i--) {
+        dishes.splice(pop_i[i], 1);
+    }
+    // Legger til riktig antall retter i listen
+    for (i = 0; i < ammount; i++) {
+        dishes.push(getFoodById(id));
+    }
+    console.log(dishes);
+}
 
-    function modalTakeaway() {
-        // Get the modal
-        var modalTakeawayElem = document.getElementById("modalTakeawayElem");
-        // Get the <span> element that closes the modal
-        var span = document.getElementsByClassName("close")[0];
-        // When the user clicks the button, open the modal
-        modalTakeawayElem.style.display = "block";
-        // When the user clicks on <span> (x), close the modal
-        span.onclick = function() {
-                modalTakeawayElem.style.display = "none";
-            }
-            // When the user clicks anywhere outside of the modal, close it
-        window.onclick = function(event) {
-            if (event.target == modalTakeawayElem) {
-                modalTakeawayElem.style.display = "none";
-            }
+function getFoodById(id) {
+    for (food of antipasti) {
+        if (food.id == id) {
+            return food
         }
     }
+    for (food of paste) {
+        if (food.id == id) {
+            return food
+        }
+    }
+    for (food of dolci) {
+        if (food.id == id) {
+            return food
+        }
+    }
+    for (food of pizze) {
+        if (food.id == id) {
+            return food
+        }
+    }
+}
